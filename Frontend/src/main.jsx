420d07ccf6af077af42b7fe58ee3b298367ae11a
amaya2509 <amayafernando150@gmail.com>
2025-04-17 10:56:59 +0530

    fix: update import for ReactDOM in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter> {/* ✅ Add this */}
       <AuthProvider>
